b656a8a931d79559c318a9fb1104a6026ff0363a
Ihago <ihagog12@gmail.com>
2020-01-06 21:43:03 -0300

    Layout tela Home

--- LudkeLaravel/resources/views/home.blade.php
+++ LudkeLaravel/resources/views/home.blade.php
@@ -3,21 +3,110 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        {{-- Linha 1 --}}
+        <div class="row justify-content-center">
+            {{-- Card Produtos --}}
+            <div class="col-sm-4">
+                <div id="card">
+                    <a id="link-card" href="{{ route("produtos")}}">
+                        <div class="row justify-content-center">
+                            <img id="card-image" src="{{ asset("img/produtos.png")  }}" alt="">
                         </div>
-                    @endif
-
-                    You are logged in!
+                        <div class="row justify-content-center">
+                            <div id="card-text">Produtos</div>
+                        </div>
+                    </a>
                 </div>
             </div>
+            
+            {{-- Card Categorias --}}
+            <div class="col-sm-4">
+                <div id="card">
+                    <a id="link-card" href="{{ route("produtos")}}">
+                        <div class="row justify-content-center">
+                            <img id="card-image-categorias" src="{{ asset("img/categorias.png")  }}" alt="">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div id="card-text">Categorias</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
+            {{-- Card 3 --}}
+            {{-- Desabilitado --}}
+            <div class="col-sm-4">
+                <div id="card" style="background-color:#aaa; box-shadow: none" onclick="alerta()">
+                    <a id="link-card" href="">
+                        <div class="row justify-content-center">
+                            <img id="card-image" src="{{ asset("img/clientes.png")  }}" alt="">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div id="card-text">Clientes</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
+        </div>
+        
+        {{-- Linha 2 --}}
+        <div class="row justify-content-center">
+            {{-- Card 1 --}}
+            {{-- Desabilitado --}}
+            <div class="col-sm-4">
+                <div id="card" style="background-color:#aaa; box-shadow: none" onclick="alerta()">
+                    <a id="link-card" href="">
+                        <div class="row justify-content-center">
+                            <img id="card-image" src="{{ asset("img/funcionarios.png")  }}" alt="">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div id="card-text">Funcionários</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
+            {{-- Card 2 --}}
+            {{-- Desabilitado --}}
+            <div class="col-sm-4">
+                <div id="card" style="background-color:#aaa; box-shadow: none" onclick="alerta()">
+                    <a id="link-card" href="">
+                        <div class="row justify-content-center">
+                            <img id="card-image" src="{{ asset("img/cargos.png")  }}" alt="">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div id="card-text">Cargos</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
+            {{-- Card 3 --}}
+            {{-- Desabilitado --}}
+            <div class="col-sm-4">
+                <div id="card" style="background-color:#aaa; box-shadow: none" onclick="alerta()">
+                    <a id="link-card" href="">
+                        <div class="row justify-content-center">
+                            <img id="card-image" src="{{ asset("img/lojas.png")  }}" alt="">
+                        </div>
+                        <div class="row justify-content-center">
+                            <div id="card-text">Lojas</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
+@endsection
+
+@section('javascript')
+<script>
+    function alerta(){
+        alert("Funcionalidade em desenvolvimento");
+    }
+</script>
 @endsection
