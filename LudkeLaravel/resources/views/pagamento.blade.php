90280080351d6d145376656e23f34c3bd2bea245
Ihago <ihagog12@gmail.com>
2020-07-25 13:56:34 -0300

    edição forma de pagamento + helper-pagamento.js

--- LudkeLaravel/resources/views/pagamento.blade.php
+++ LudkeLaravel/resources/views/pagamento.blade.php
@@ -174,23 +174,15 @@
 
 @section('javascript')
 
+<script src="{{ asset('js/helper-pagamento.js') }}"></script>
+
 <script type="text/javascript">
 
-    let countFormaPagamento = 0;
+    const today = "<?php date_default_timezone_set('America/Sao_Paulo'); echo date('Y-m-d');?>";
+    
     //Formas de pagamento
     let formasPagamento = <?php echo json_encode($formasPagamento); ?>; 
     
-    
-
-    function validPayment(contValorTotalPagamento, valorTotal){
-
-        if( contValorTotalPagamento < valorTotal){
-            return false;
-        }else{
-            return true;
-        }
-
-    }
 
     $(function(){
         $('#formPagamento').submit(function(event){
@@ -224,7 +216,7 @@
                     alert("O valor informado é menor do que o valor total! Uma nova forma de pagamento será adicionada.")
 
                     // Adiciona nova forma de pagamento ao formulário
-                    var linhaForm = addFormaDePagamento();
+                    var linhaForm = addFormaDePagamento(today);
                     $("#divNovaFormaPagamento").append(linhaForm);
 
                     // Adiciona o valor restante em na ultima forma de pagamento adicionada
@@ -240,38 +232,14 @@
         // Ao clicar no botão "Adicionar Forma de Pagamento" Adiciona na tela os inputs da nova forma de pagamento
         $('#bntNovaFormaPagamento').click(function(){
             // alert('Adicionar Forma de pagamento')
-            var linhaForm = addFormaDePagamento();
+            var linhaForm = addFormaDePagamento(today);
             $("#divNovaFormaPagamento").append(linhaForm);
         });
 
         
     });
-    //Função que desabilita data de vencimento quando o tipo de pagamento for "à vista"
-    function disableDataVencimento(select){
-        // Caso o pagamento seja 'a vista'
-        let rowForm = select.parentElement.parentElement;
-        let colDate = rowForm.childNodes[3];
-        let inputDate = colDate.childNodes[1];
-        
-        if(select.value == 1){
-            inputDate.required = false;
-            colDate.style.display = "none";
-            let date = new Date();
-            let now = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + '-' + date.getDate().toString().padStart(2, 0);
-            inputDate.value = now;
-            
-        }else{
-            inputDate.required = true;
-            inputDate.value = '';
-            colDate.style.display = "block";
-            
-        }
-    }
-    // Ao clicar no botão excluir, retira os inputs referente à forma de pagamento
-    function excluirFormaPagamento(id){
-        id = "formaPagamento"+id;
-        $(`#${id}`).remove();
-    }
+    
+
     // Função que percorre os valores de entrada nos pagamentos e verifica se é maior que o valor do 
     //pedido.
     function validaValorPagamento(){
@@ -299,54 +267,6 @@
         }
     }
     
-    // Cria a linha com os inputs da nova forma de pagamento
-    function addFormaDePagamento(){
-        
-        // console.log("Nova Forma de Pagamento")
-        let form = "<div id='formaPagamento"+countFormaPagamento+"'>"+
-                    "<div class='row informacoes'>"+
-                        "<div class='col-sm-10'>"+
-                            "<h3>Informações do Pagamento</h3>"+
-                        "</div>"+
-                        "<div class='col-sm-2'>"+
-                            "<button class='btn btn-secondary-ludke' onclick='excluirFormaPagamento("+countFormaPagamento+")'>Excluir</button>"+
-                        "</div>"+
-                    "</div>"+
-                    "<div class='row justify-content'>"+
-                        "<div class='col-sm-3 form-group'>"+
-                            "<label for='formaPagamento'>Tipo de Pagamento <span class='obrigatorio'>*</span></label>"+
-                            "<select name='formaPagamento[]' class='form-control' id='formaPagamento' onChange='disableDataVencimento(this)' required>"+
-                                "<option value='' disabled>-- Tipo de Pagamento --</option>"+
-                                optionsFormaPagamento()+
-                            "</select>"+
-                            "<span style='color:red' id='spanformaPagamento'></span>"+
-                        "</div>"+
-                        "<div class='col-sm-3 form-group'>"+
-                            "<label for='valorTotalPagamento'>Valor (R$) <span class='obrigatorio'>*</span></label>"+
-                            "<input type='number' id='valorTotalPagamento' min='0' step='0.01' onkeyup='validaValorPagamento()' class='form-control' name='valorTotalPagamento[]' required>"+
-                            "<span style='color:red' id='spanValorPago'></span>"+
-                        "</div>"+
-                        "<div class='col-sm-3 form-group'>"+
-                            "<label for='descontoPagamento'>Desconto %</label>"+
-                            "<input id='descontoPagamento' type='number' class='form-control' value='0' min='0' max='100' name='descontoPagamento[]' disabled>"+
-                            "<span style='color:red' id='spanDescontoPagamento'></span>"+
-                        "</div>"+
-                        "<div class='col-sm-3 form-group' style='display: none'>"+
-                            "<label for='dataVencimento'>Data de Vencimento</label>"+
-                            "<input type='date' class='form-control' id='dataVencimento' name='dataVencimento[]' value='<?php date_default_timezone_set('America/Sao_Paulo'); echo date('Y-m-d');?>'>"+
-                            "<span style='color:red' id='spanDataVencimento'></span>"+
-                        "</div>"+
-                    "</div>"+                    
-                    "<div class='row justify-content-center'>"+
-                        "<div class='col-sm-12 form-group'>"+
-                            "<label for='obs'>Observações</label>"+
-                            "<textarea class='form-control' name='obs[]' id='' rows='5'></textarea>"+
-                        "</div>"+
-                    "</div>"+
-                "</div>";
-        countFormaPagamento += 1;
-        return form;           
-    }
     // monta as linhas dos <option> com as formas de pagamento para serem exibidas no select
     function optionsFormaPagamento(){
         let options = "";
